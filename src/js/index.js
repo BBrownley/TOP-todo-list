af99e8844e8244aa2510251ca590bf639a89dfa4
BBrownley <brendobrownley@yahoo.com>
2020-03-01 00:15:11 -0500

    Improved UI, Bug fixes

--- src/js/index.js
+++ src/js/index.js
@@ -50,42 +50,47 @@ Todo (B) - Displays the following:
 
 // NOTES
 
--There is an "Add todo" button in the project window
--User clicks "Add todo"
--Form appears, info to be filled out for the new todo object
--User submits form, new todo is added to the currently selected project
+-User clicks on todo
+-Box appears on screen, listing all its details
+
+-User clicks delete on todo
+-Todo is removed from the project
 
 // BUGS
 
--Empty forms can be submitted
+-Form validation errors don't disappear after submitting them
 -User can tab over to buttons that are on the z-index baseline while form is open
+-Todo counts on project tabs not updating after todo is added
 
 */
 
 import Project from "./Project";
+import Todo from "./Todo";
+
+const moment = require("moment");
 
 const projects = [];
 
 const project1 = new Project("My first project");
-project1.addTodo("Do homework");
-project1.addTodo("Wash the dishes");
-project1.addTodo("Busy work");
+project1.addTodo(new Todo("Do homework", "", "", "Low", "3"))
+project1.addTodo(new Todo("Wash the dishes", "", "", "Very Low", "20"))
+project1.addTodo(new Todo("Busy work", "", "", "Very High", "60"))
 
 projects.push(project1);
 
 const project2 = new Project("Another project");
-project2.addTodo("Take out trash");
-project2.addTodo("Work out");
+project2.addTodo(new Todo("Take out trash", "", "", "Medium", "30"))
+project2.addTodo(new Todo("Work out", "", "", "High", "2"))
 
 projects.push(project2);
 
 require("../scss/main.scss");
 
-const events = (() => {
+const dataStorage = (() => {
 
+  
 
-
-})
+})();
 
 const DOM = (() => {
 
@@ -115,11 +120,31 @@ const DOM = (() => {
 
   }
 
-  const renderTodosFromProject = project => {
+  const colorTodoPriority = priority => {
+    switch(priority) {
+      case "Very Low":
+        return "green";
+      case "Low":
+        return "yellowgreen";
+      case "Medium":
+        return "yellow";
+      case "High":
+        return "orange";
+      case "Very High":
+        return "red";
+      default:
+        return "black";
+    }
+  }
+
+  // project - represents an Object of type Project
+  const renderTodosFromProject = (project, projectIndex) => {
 
     const todosContainer = document.querySelector(".todos-container");
 
     todosContainer.innerHTML = "";
+
+    let todoIndex = 0; // Assign each rendered todo to a data-index so we can locate them later
 
     if (project.todos.length === 0) {
       const message = document.createElement("p");
@@ -127,11 +152,25 @@ const DOM = (() => {
       todosContainer.appendChild(message);
     } else {
       project.todos.forEach(todo => {
-        const todoElement = document.createElement("p");
-        todoElement.classList.add("todo");
-        todoElement.textContent = todo;
-        todoElement.style.color = "blue";
-        todosContainer.appendChild(todoElement);
+
+        const todoElement = 
+
+        `
+          <div class="todo" data-todo-index=${todoIndex} data-child-of-project-at-index=${projectIndex} style="border-left: 5px solid ${colorTodoPriority(todo.priority)}">
+            <div class="todo-info">
+                <p class="todo-title">${todo.title}</p>
+                <span class="todo-duration">(${parseInt(todo.estimatedTime)} minutes)</span>
+                <span class="todo-priority"><div class="todo-priority-marker" style="background-color: ${colorTodoPriority(todo.priority)}"></div>${todo.priority} priority</span>
+            </div>
+            <div class="todo-options">
+                <button class="edit-todo">Edit</button>
+                <button class="delete-todo">Delete</button>
+            </div>
+          </div>
+        `
+
+        todosContainer.innerHTML += todoElement;
+        todoIndex++;
       })
     }
 
@@ -173,6 +212,16 @@ const DOM = (() => {
     });
   }
 
+  const showTodoDetails = todoElement => {
+    console.log(todoElement);
+
+
+    // These two variables are essentially "coordinates" for locating the selected todo in data
+    const projectIndex = todoElement.getAttribute("data-child-of-project-at-index");
+    const todoIndex = todoElement.getAttribute("data-todo-index");
+
+  }
+
   const closeFormButtons = Array.from(document.getElementsByClassName("close-form-button"));
   closeFormButtons.forEach(button => button.addEventListener("click", closeForm));
 
@@ -191,7 +240,8 @@ const DOM = (() => {
     markSelectedProject, 
     openForm,
     resetForm,
-    closeForm
+    closeForm,
+    showTodoDetails
   };
 
 })();
@@ -199,6 +249,8 @@ const DOM = (() => {
 const controller = (() => {
 
   const allProjects = projects;
+
+  let selectedProjectIndex = 0;
 
   const updateProject = () => {
 
@@ -208,12 +260,19 @@ const controller = (() => {
 
   }
 
+  const getSelectedProjectIndex = () => {
+    return Array.from(document.getElementsByClassName("project")).find(project => {
+      return project.classList.contains("selected")
+    }).getAttribute("data-project-index");
+  }
+
+  // project - Represents a project tab element
   const selectProject = project => {
 
       const projectIndex = project.getAttribute("data-project-index");
 
       DOM.markSelectedProject(project);
-      DOM.renderTodosFromProject(projects[projectIndex]);
+      DOM.renderTodosFromProject(projects[projectIndex], projectIndex);
 
   }
 
@@ -239,6 +298,24 @@ const controller = (() => {
   // ¯\_(ツ)_/¯
 
   const submitForm = form => {
+
+    const formValidationErrors = validateForm(form);
+
+    if (formValidationErrors.length > 0) {
+
+      const formErrorContainer = form.querySelector(".form-errors");
+      formErrorContainer.innerHTML = "";
+
+      formValidationErrors.forEach(err => {
+        const errorElement = document.createElement("p");
+        errorElement.textContent = err;
+        formErrorContainer.appendChild(errorElement);
+      });
+
+      return;
+    }
+
+    // Extract values from form
     if (form.getAttribute("id") === "new-project-form") {
       const newProjectName = document.getElementById("projectName").value;
       createProject(newProjectName);
@@ -246,17 +323,55 @@ const controller = (() => {
       DOM.resetForm(form);
     } else if (form.getAttribute("id") === "add-todo-form") {
 
-      const todoProperties = {};
+      const todoProperties = [];
 
-      todoProperties.title = form["todoTitle"].value;
-      todoProperties.description = form["todoDescription"].value;
-      todoProperties.dueDate = form["todoDueDate"].value;
-      todoProperties.priority = document.querySelector(`input[name="priority"]:checked`).value;
-      todoProperties.estimatedTime = form["todoEstimatedTime"].value;
+      todoProperties[0] = form["todoTitle"].value;
+      todoProperties[1] = form["todoDescription"].value;
+      todoProperties[2] = form["todoDueDate"].value;
+      todoProperties[3] = document.querySelector(`input[name="priority"]:checked`).value;
+      todoProperties[4] = form["todoEstimatedTime"].value;
 
-      console.log(todoProperties)
+      const newTodo = new Todo(...todoProperties);
+
+      // Get selected project and add the new todo to it
+      selectedProjectIndex = getSelectedProjectIndex();
+      allProjects[selectedProjectIndex].addTodo(newTodo);
+
+      // Close/Reset form, render updated list of todos
+      DOM.closeForm();
+      DOM.resetForm(form);
+      DOM.renderTodosFromProject(allProjects[selectedProjectIndex], selectedProjectIndex);
+
+      // Updates the count displayed on project tab
+      DOM.renderProjects(allProjects);
 
     }
+
+  }
+
+  // form - Represents the HTML form
+  const validateForm = form => {
+    
+    const errors = [];
+
+    console.log(form.elements)
+
+    const formInputsToValidate = Array.from(form.elements).filter(formElement => {
+      return formElement.type === "text" || formElement.type === "number";
+    });
+    
+    formInputsToValidate.forEach(textInput => {
+      if (textInput.value.trim().length === 0) {
+
+        errors.push(`${textInput.name} not specified`)
+
+        textInput.style.border = "2px solid red";
+
+      }
+    })
+
+    return errors;
+
   }
 
   document.addEventListener("click", e => {
@@ -270,14 +385,15 @@ const controller = (() => {
       selectProject(DOM.checkIfTargetWithinElement(e.target, ".project"));
 
     } else if (DOM.checkIfTargetWithinElement(e.target, ".todo")) {
-      console.log("todo clicked")
+      DOM.showTodoDetails(DOM.checkIfTargetWithinElement(e.target, ".todo"))
     }
 
   })
 
   window.addEventListener("load", () => {
     DOM.renderProjects(allProjects);
-    selectProject(document.querySelector(".project"));
+    //selectProject(document.querySelector(".project"));
+    document.getElementById("todoDueDate").value = moment(new Date()).add(1, "days").format("YYYY-MM-DD");
   })
 
   return {submitForm}
